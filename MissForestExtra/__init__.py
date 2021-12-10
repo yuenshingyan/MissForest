4814eec0fb61178ee356b546d6b8c13b42ac31c8
Hindy <69097028+HindyDS@users.noreply.github.com>
2021-12-10 09:59:35 +0800

    Refactored the code and added comments

--- MissForestExtra/__init__.py
+++ MissForestExtra/__init__.py
@@ -10,78 +10,93 @@ class MissForestExtra:
     def __init__(self):     
         self.__author__ = 'Yuen Shing Yan Hindy'
         self.__license__= 'MIT License'
-        self.__contact__ = 'https://github.com/HindyDS/MissForest'
+        self.__contact__ = 'https://github.com/HindyDS/MissForestExtra'
     
-    def single_impute(self, x, impute, model, max_iter=5, verbose=0):
-        x3 = x.copy()
-        pd.options.mode.chained_assignment = None  # default='warn'
+    def single_impute(self, x, feature_to_be_imputed, estimator, max_iter=5):
+        # Disable pandas warning
+        pd.options.mode.chained_assignment = None
+        x2 = x.copy()    
                   
-        encode_col = [f for f in x.columns if isinstance(x[f].sample(1).values[0], str)]
-        for c in encode_col:  
-            x3[c].replace(x[c].dropna().unique(), range(x[c].dropna().nunique()), inplace=True) # label encoding
-            if c == impute:
-                impute_map = ({k:v for (k,v) in zip(x[c].dropna().unique(), range(x[c].dropna().nunique()))})
-                impute_map = {v:k for (k,v) in impute_map.items()}
-       
-        non_null = list(set([feat for feat, val in zip(x.isnull().sum().index, x.isnull().sum()) if val == 0] + [impute]))
-            
-        track = {}
-        for n_iter in range(max_iter):
-            x2=x3[non_null].copy()
-            x2['Training/Predict'] = np.where(x2[impute].isnull(), 'predict', 'training')
-            predict_set = x2[x2['Training/Predict'] == 'predict']
-            training_set = x2[x2['Training/Predict'] == 'training']
+        columns_needed_to_be_encoded = [column for column in x.columns if isinstance(x[column].sample(1).values[0], str)]
+        # If feature in x is string, then label encoding the feature
+        for column in columns_needed_to_be_encoded:
+          non_na_categories = x[column].dropna().unique()
+          labels = range(x[column].dropna().nunique())
+          x2[column].replace(non_na_categories, labels, inplace=True)
 
-            if n_iter == 1:
-                predict_set[impute].fillna(x2[impute].median(), inplace=True) if impute in encode_col else predict_set[impute].fillna(x2[impute].mode().values[0], inplace=True)
-                   
-            if n_iter > 1:
-                for idx, pred in zip(predict_set.index, res):
-                    predict_set.loc[idx, impute] = pred
-           
-            model.fit(training_set[non_null].drop(impute, axis=1), training_set[impute])
-            if predict_set[non_null].drop(impute, axis=1).shape[0] != 0:
-                res = model.predict(predict_set[non_null].drop(impute, axis=1))
-            else:
-                raise Exception(f'Feature "{impute}" does not contain any missing values.')
-                
-            track[f'iter{n_iter}'] = res
+          if column == feature_to_be_imputed:
+              impute_map = {label:category for category, label in zip(non_na_categories, labels)}
+       
+        non_null_features = list(set([feat for feat, val in zip(x.isnull().sum().index, x.isnull().sum()) if val == 0] + [feature_to_be_imputed]))
             
-            if n_iter > 1:       
-                if (track[f'iter{n_iter}'] == track[f'iter{n_iter - 1}']).all():
-                    if verbose == 1:
-                        print('Stopping Criteria Reached (no change in imputed values)')
-                    break
-            if verbose == 1:       
-                if max_iter <= 1:
-                    print('Stopping Criteria Reached (max iter reached)')
-                    break
+        x2 = x2[non_null_features].copy()
+        for i in range(max_iter):
+            x2['Training/Predict'] = np.where(x2[feature_to_be_imputed].isnull(), 'predict', 'training')
+            predict = x2[x2['Training/Predict'] == 'predict']
+            training = x2[x2['Training/Predict'] == 'training']
+
+            # If it is first iteration
+            if i == 1:
+              # The feature is numeric, then impute it with the median
+              if feature_to_be_imputed in columns_needed_to_be_encoded:
+                feature_median = x2[feature_to_be_imputed].median()
+                predict[feature_to_be_imputed].fillna(feature_median, inplace=True)
+
+              # The feature is string, then impute it with the mode
+              else:
+                feature_mode = x2[feature_to_be_imputed].mode().values[0]
+                predict[feature_to_be_imputed].fillna(feature_mode, inplace=True)
                    
-            n_iter += 1
+            if i > 1:
+                for idx, pred in zip(predict.index, y_miss_pred):
+                    predict.loc[idx, feature_to_be_imputed] = pred
+           
+            # Train the estimator
+            X_train = training[non_null_features].drop(feature_to_be_imputed, axis=1)
+            y_train = training[feature_to_be_imputed]
+            estimator.fit(X_train, y_train)
+
+            if predict[non_null_features].drop(feature_to_be_imputed, axis=1).shape[0] != 0:
+                y_miss_pred = estimator.predict(predict[non_null_features].drop(feature_to_be_imputed, axis=1))
+
+            else:
+                raise Exception(f'No missing values in "{feature_to_be_imputed}".')
+
+            i += 1
            
         pd.options.mode.chained_assignment = 'warn'  
        
-        imputed_feature = pd.concat([training_set[impute], predict_set[impute]], axis=0).sort_index()
-        return imputed_feature.replace(impute_map) if impute in encode_col else imputed_feature
+        # Combine the predictions with the non-imputed values and sort them according to the original index
+        imputed_feature = pd.concat([training[feature_to_be_imputed], predict[feature_to_be_imputed]], axis=0).sort_index()
 
-    def impute(self, x, classifier, regressor, max_iter=5, verbose=0):
+        if feature_to_be_imputed in columns_needed_to_be_encoded:
+          imputed_feature.replace(impute_map, inplace=True)
+
+        return imputed_feature
+
+    def impute(self, x, classifier, regressor, max_iter=5):
       x = x.copy()
-      x2 = x.copy()
-      columns_that_require_imputation_in_ascending_order = x.isnull().sum()[x.isnull().sum() != 0].sort_values().index
+      imputed_x = x.copy()
+      features_that_require_imputation_in_ascending_order = x.isnull().sum()[x.isnull().sum() != 0].sort_values().index
 
+      # Check which feature is string
       is_string = {}
-      for col in columns_that_require_imputation_in_ascending_order:
-        is_string[col] = isinstance(x[~x[col].isnull()].loc[:, col].sample(1).values[0], str)
-        
-      for col in columns_that_require_imputation_in_ascending_order:
-        if is_string[col]:
-          imputed = self.single_impute(x, col, classifier, max_iter, verbose)
+      for feature in features_that_require_imputation_in_ascending_order:
+        feature_sample = x[~x[feature].isnull()].loc[:, feature].sample(1).values[0]
+        is_string[feature] = isinstance(feature_sample, str)
+      
+      # If feature is string, impute it with a classifier.
+      # If feature is numeric, impute it with a regressor
+      for feature in features_that_require_imputation_in_ascending_order:      
+        if is_string[feature]:
+          imputed_feature = self.single_impute(x, feature, classifier, max_iter)
 
         else:
-          imputed = self.single_impute(x, col, regressor, max_iter, verbose) 
+          imputed_feature = self.single_impute(x, feature, regressor, max_iter) 
 
-        insert_index = list(x.columns).index(col)
-        x2.drop(col, axis=1, inplace=True)
-        x2.insert(insert_index, col, imputed)  
+        # Drop original features and Insert imputed features using the correct indexes
+        insert_index = list(x.columns).index(feature)
+        imputed_x.drop(feature, axis=1, inplace=True)
+        imputed_x.insert(insert_index, feature, imputed_feature)  
 
-      return x2
+      return imputed_x
