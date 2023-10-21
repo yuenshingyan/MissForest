e2b31644f5000e2a871a5cfbc9f42317e24284ae
Hindy <yuenshingyan@gmail.com>
2023-10-21 17:41:10 +0800

    update docstring, refactor code and add new error MultipleDataTypeError

--- missforest/errors.py
+++ missforest/errors.py
@@ -1,11 +1,19 @@
 """This module contains Python class 'MissForest'."""
 
-__all__ = ["InputInvalidError"]
+__all__ = ["InputInvalidError", "MultipleDataTypesError"]
 __version__ = "2.0.0"
 __author__ = "Yuen Shing Yan Hindy"
 
 
 class InputInvalidError(Exception):
     """'InputInvalidError' is raised when the input argument 'X' is not
-    either pandas dataframe, numpy array or list of lists."""
+    either pandas dataframe, numpy array or list of lists when calling class
+    method '_check_if_valid'."""
+    pass
+
+
+class MultipleDataTypesError(Exception):
+    """'MultipleDataTypesError' is raised when any of the columns the input
+    argument 'X' has more one datatype when calling class method
+    '_check_if_all_single_type'."""
     pass
