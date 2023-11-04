bfca71821f65575d7e5da3bc03f761e0ea4af907
yuenshingyan <yuenshingyan@gmail.com>
2023-11-04 16:01:36 +0800

    update version

--- src/missforest/missforest.py
+++ src/missforest/missforest.py
@@ -1,7 +1,7 @@
 """This module contains Python class 'MissForest'."""
 
 __all__ = ["MissForest"]
-__version__ = "2.2.0"
+__version__ = "2.2.2"
 __author__ = "Yuen Shing Yan Hindy"
 
 from copy import deepcopy
