3b5ab6526c2b88f1390fe1ad0ae5c7b9f28fbee8
Hindy <yuenshingyan@gmail.com>
2024-12-16 10:33:18 +0800

    update `_validate_column_consistency`

--- tests/unit_tests/validate_column_consistency.py
+++ tests/unit_tests/validate_column_consistency.py
@@ -9,12 +9,16 @@ from src.missforest._validate import _validate_column_consistency
 class ValidateColumnConsistency(unittest.TestCase):
     @staticmethod
     def test_validate_column_consistency():
+        """Tests if no ValueError is raised if `existing` and `new` are
+        consistent."""
         existing = {1, 2, 3}
         new = {1, 2, 3}
         _validate_column_consistency(existing, new)
 
     def test_validate_column_consistency_negative(self):
+        """Tests if ValueError is raised if `existing` and `new` are not
+        consistent."""
         with self.assertRaises(ValueError):
             existing = {1, 2, 3}
-            new = {1, 2, }
+            new = {1, 2}
             _validate_column_consistency(existing, new)
